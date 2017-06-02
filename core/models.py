68ee2c76882edcda3e594058b8e6bcda60d1776b
c0de <c0de@c0defox.es>
2017-06-01 20:31:56 -0500

    Flesh out schema more
    This adds proper unicode support
    api_owners now includes date_added, active, date_deactivated
    This also defines the __str__ functions for internal django usage

--- core/models.py
+++ core/models.py
@@ -1,16 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from  django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class api_owners(models.Model):
 	# Users who are allowed to have an API app
-	user = models.ForeignKey(User, unique=True, blank=False)
+	user = models.OneToOneField(User, unique=True, blank=False)
+	date_added = models.DateField(blank=False)
+	date_deactivated = models.DateField()
+	active = models.BooleanField(blank=False)
+
+	def __str__(self):
+		return '%s (%s)' % (self.user.username, self.user.id)
 
 
+@python_2_unicode_compatible
 class api_apps(models.Model):
 	# An Application registered with our API
 	owner = models.ForeignKey('api_owners', blank=False)
@@ -21,7 +30,11 @@ class api_apps(models.Model):
 	callback_uri = models.URLField(max_length=3000)
 	settings = JSONField()
 
+	def __str__(self):
+		return '%s (%s) - %s' % (self.name, self.owner.name, self.apikey)
 
+
+@python_2_unicode_compatible
 class api_heartbeat(models.Model):
 	# If callback_uri is configured and heartbeat is enabled in settings,
 	# this can be used to 'ping' the application, as well as communicate
@@ -33,3 +46,6 @@ class api_heartbeat(models.Model):
 	time_rcvd = models.DateTimeField(blank=False)
 	latency = models.FloatField(blank=False)
 	data = JSONField()
+
+	def __str__(self):
+		return '%s (%s ms)' % (self.api.name, self.latency)
